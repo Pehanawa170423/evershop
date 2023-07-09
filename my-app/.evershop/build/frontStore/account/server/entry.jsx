aaf89e416849121b05187f86c897a0cc13ad290b
Pehanawa170423 <135044172+Pehanawa170423@users.noreply.github.com>
2023-07-09 22:38:06 +0530

    changes added for customer creation
    added fields to Createcustomer

---
+++ my-app/.evershop/build/frontStore/account/server/entry.jsx
@@ -0,0 +1,79 @@
+import React from 'react'; 
+import ReactDOM from 'react-dom'; 
+import Area from '@evershop/evershop/src/components/common/Area';
+Area.defaultProps.components = {
+  'icon-wrapper': {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4',
+      sortOrder: 50,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/MobileMenu.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4',
+      sortOrder: 30,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
+    }
+  },
+  content: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4',
+      sortOrder: 0,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L0xheW91dC5qc3g=',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/account/Layout.jsx')
+    }
+  },
+  body: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=',
+      sortOrder: 1,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
+    }
+  },
+  header: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4',
+      sortOrder: 1,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4',
+      sortOrder: 5,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Menu.jsx')
+    }
+  },
+  head: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01ldGEuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01ldGEuanN4',
+      sortOrder: 5,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Meta.jsx')
+    }
+  },
+  accountPageRight: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L0FjY291bnREZXRhaWxzLmpzeA==': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L0FjY291bnREZXRhaWxzLmpzeA==',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/account/AccountDetails.jsx')
+    }
+  },
+  accountPageLeft: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L09yZGVySGlzdG9yeS5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hY2NvdW50L09yZGVySGlzdG9yeS5qc3g=',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/account/OrderHistory.jsx')
+    }
+  }
+} 
---
+++ my-app/.evershop/build/frontStore/homepage/client/entry.jsx
@@ -0,0 +1,82 @@
+
+      import React from 'react';
+      import ReactDOM from 'react-dom';
+      import Area from '@evershop/evershop/src/components/common/Area';
+      import Hydrate from '@evershop/evershop/src/components/common/react/client/HydrateFrontStore';
+      
+Area.defaultProps.components = {
+  content: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2hvbWVwYWdlL0ZlYXR1cmVkQ2F0ZWdvcmllcy5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2hvbWVwYWdlL0ZlYXR1cmVkQ2F0ZWdvcmllcy5qc3g=',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/homepage/FeaturedCategories.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2hvbWVwYWdlL0ZlYXR1cmVkUHJvZHVjdHMuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2F0YWxvZy9wYWdlcy9mcm9udFN0b3JlL2hvbWVwYWdlL0ZlYXR1cmVkUHJvZHVjdHMuanN4',
+      sortOrder: 15,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/catalog/pages/frontStore/homepage/FeaturedProducts.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0JyZWFkY3J1bWIuanN4',
+      sortOrder: 0,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Breadcrumb.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvaG9tZXBhZ2UvTWFpbkJhbm5lci5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvaG9tZXBhZ2UvTWFpbkJhbm5lci5qc3g=',
+      sortOrder: 1,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/homepage/MainBanner.jsx')
+    }
+  },
+  'icon-wrapper': {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY2hlY2tvdXQvcGFnZXMvZnJvbnRTdG9yZS9hbGwvTWluaUNhcnQuanN4',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/checkout/pages/frontStore/all/MiniCart.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01vYmlsZU1lbnUuanN4',
+      sortOrder: 50,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/MobileMenu.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY3VzdG9tZXIvcGFnZXMvZnJvbnRTdG9yZS9hbGwvVXNlckljb24uanN4',
+      sortOrder: 30,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/customer/pages/frontStore/all/UserIcon.jsx')
+    }
+  },
+  body: {
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xheW91dC5qc3g=',
+      sortOrder: 1,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx')
+    },
+    'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=': {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL05vdGlmaWNhdGlvbi5qc3g=',
+      sortOrder: 10,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Notification.jsx')
+    }
+  },
+  header: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL0xvZ28uanN4',
+      sortOrder: 1,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Logo.jsx')
+    },
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01lbnUuanN4',
+      sortOrder: 5,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Menu.jsx')
+    }
+  },
+  head: {
+    L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01ldGEuanN4: {
+      id: 'L25vZGVfbW9kdWxlcy9AZXZlcnNob3AvZXZlcnNob3Avc3JjL21vZHVsZXMvY21zL3BhZ2VzL2Zyb250U3RvcmUvYWxsL01ldGEuanN4',
+      sortOrder: 5,
+      component: require('/Users/bkumar/evershop/my-app/node_modules/@evershop/evershop/src/modules/cms/pages/frontStore/all/Meta.jsx')
+    }
+  }
+} 
+ReactDOM.hydrate(
+        <Hydrate/>,
+        document.getElementById('app')
+      );
